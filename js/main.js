dbafd9b2b3219c9c81e08521d6ff33e4550e18cc
Pratiti Bera <remabimal0801@gmail.com>
2021-12-11 13:26:16 +0530

    Blogs section added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+
 $(document).ready(function () {
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
     // AOS Instance
     AOS.init();
 
