710b55a33bf72253420caf566d8a06a185fe5864
Pratiti Bera <remabimal0801@gmail.com>
2021-12-11 15:03:11 +0530

    -

--- js/main.js
+++ js/main.js
@@ -15,6 +15,13 @@ const responsive = {
 
 
 $(document).ready(function () {
+     // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+    
     // AOS Instance
     AOS.init();
 });
