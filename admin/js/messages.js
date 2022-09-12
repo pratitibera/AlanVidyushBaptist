6cb6f8f0963a0f82ef8fee7a149dfab0a0ce1924
Pratiti Bera <remabimal0801@gmail.com>
2022-09-12 10:45:21 +0530

    -

--- admin/js/messages.js
+++ admin/js/messages.js
@@ -1,4 +1,4 @@
-function viewMessages(){
+function viewoptiMessages(){
 	var request = new XMLHttpRequest();
 	request.open(urlSet.getMessagesApi.method, urlSet.getMessagesApi.url, true);
 	request.setRequestHeader("Accept", "application/json");
@@ -15,8 +15,7 @@ function viewMessages(){
 			});
 			var td1 = document.createElement('td');
 			td1.append(istDate);
-			//td1.append("Date");
-
+			
 			var td2 = document.createElement('td');
 			td2.append(data[i]['name']);
 
@@ -58,10 +57,56 @@ function deleteMessages(id){
 		$('#confirmDeletion').modal('hide');
 		if(data['message'] == "Message Deleted"){
 			alert("Message has been deleted.");
-			viewMessages();
+			viewoptiMessages();
 		}
 		else{
 			alert("Could not delete this message.");
+		}
+	}
+}
+
+function viewpwMessages(){
+	var request = new XMLHttpRequest();
+	request.open(urlSet.getMessagesApi.method, urlSet.getMessagesApi.url, true);
+	request.setRequestHeader("Accept", "application/json");
+	request.send();
+	request.onload = function () {
+		var data = JSON.parse(this.response);
+		console.log(data);
+		var message_list = document.getElementById('message_list_pw');
+		message_list.innerHTML = "";
+
+		for(i = 0; i < data.length; i++){
+			var istDate = new Date(data[i]['createdAt']).toLocaleString(undefined, {
+				timeZone: 'Asia/Kolkata'
+			});
+			var td1 = document.createElement('td');
+			td1.append(istDate);
+			
+			var td2 = document.createElement('td');
+			td2.append(data[i]['name']);
+
+			var td3 = document.createElement('td');
+			td3.append(data[i]['phone']);
+
+			var td4 = document.createElement('td');
+			td4.append(data[i]['email']);
+
+			var td5 = document.createElement('td');
+			td5.append(data[i]['message']);
+
+			var td6 = document.createElement('td');
+			td6.innerHTML = `<button class="btn btn-dark" onclick="deleteHandler(this.id);" id="deleteMessage_${data[i]['_id']}">DELETE</button>`;
+
+			var tr = document.createElement('tr');
+			tr.append(td1);
+			tr.append(td2);
+			tr.append(td3);
+			tr.append(td4);
+			tr.append(td5);
+			tr.append(td6);
+
+			message_list.append(tr);
 		}
 	}
 }
